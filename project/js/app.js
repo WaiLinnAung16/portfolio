1bf67b337d09f5df780e5f04ab7a9c53a1cdc4bf
WaiLinnAung18 <waia00198@gmail.com>
2022-09-30 10:14:07 +0630

    final before animation

---
+++ project/js/app.js
@@ -0,0 +1,15 @@
+let typed = new Typed(".auto-input", {
+    strings:["Wai Linn Aung","Front-end Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});
+
+$(".project-slick").slick({
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000
+})
+
+
